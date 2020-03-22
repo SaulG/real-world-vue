28b17bd0a621b8bcf244216a766eaf62811f26f8
Saul Gausin <saul.gausin@gmail.com>
2020-03-21 23:16:17 -0600

    Inital commit

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="icon-wrapper">
+	<svg class='icon'  :width="width" :height="height">
+            <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}"/>
+	</svg>
+	<slot></slot> // newly added slot
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCreate from "../views/EventCreate.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function() {
-      return import(/* webpackChunkName: "about" */ "../views/About.vue");
+    {
+	path: "/",
+	name: "event-list",
+	component: EventList
+    },
+    {
+	path: "/event/create",
+	name: "event-create",
+	component: EventCreate
+    },    
+    {
+	path: "/event/:id",
+	name: "event-show",
+	component: EventShow,
+	props: true
     }
-  }
 ];
 
 const router = new VueRouter({
-  routes
+    mode: 'history',
+    routes
 });
 
 export default router;
